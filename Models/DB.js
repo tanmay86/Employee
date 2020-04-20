b790f919d5a0af834401fff6bda82b58120868e3
unknown <tanmay86tiwari@gmail.com>
2020-04-20 14:03:43 +0530

    changed con string

--- Models/DB.js
+++ Models/DB.js
@@ -1,7 +1,9 @@
 
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/Employee',{useNewUrlParser:true},(err)=>{
+const constringprd ='mongodb://employeemongo:mKpWOabNFOsaa6nuTYL0aUbKc0AYy4KeHqinclm4BtJWJNQ0ljmoaIyei9UKEcy29nKchEVzBwEYhALQE7pI5w%3D%3D@employeemongo.mongo.cosmos.azure.com:10255/?ssl=true&appName=@employeemongo@'
+const constringlocal ='mongodb://localhost:27017/Employee'
+mongoose.connect(constringprd,{useNewUrlParser:true},(err)=>{
 if(!err){
 
     console.log('DB connected');
